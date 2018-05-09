666256db7282eea16cbbfc92bbcd104a3f104283
Wei <yang.wei.nj.cn@gmail.com>
2018-05-09 16:35:52 +0930

    table supported

--- page_object.py
+++ page_object.py
@@ -20,19 +20,19 @@ class PageElement(object):
     PageElement are used inside a PageObject to define elements on the page.
     """
 
-    def __init__(self, loc, by=By.ID, wrapper=None, timeout=30):
+    def __init__(self, loc, by=By.ID, component=None, timeout=30):
         self.locator = loc
         self.by = by
-        self.wrapper = wrapper
+        self.component = component
         self.timeout = timeout
 
     def __get__(self, instance, owner):
         """Getting a PageElement will return the element"""
         page_logger.debug('Accessing page element {}'.format(self.locator))
         try:
-            e = self._find_element(instance.driver)
-            if self.wrapper is not None:
-                e = self.wrapper(e)
+            e = self._find_element(instance.context)
+            if self.component is not None:
+                e = self.component(e)
             return e
         except Exception as ex:
             page_logger.debug('Cannot find the element')
@@ -42,7 +42,7 @@ class PageElement(object):
     def __set__(self, instance, value):
         """Setting a PageElement will send keybord input to the element"""
         page_logger.debug('Setting page element')
-        element = self._find_element(instance.driver)
+        element = self._find_element(instance.context)
         self._set_element(element, value)
 
     def _find_element(self, driver):
@@ -86,8 +86,12 @@ class PageElement(object):
 class PageComponent(object):
 
     def __init__(self, element):
-        self.driver = element
+        self.context = element
         self.page = PageObject(element._parent)
+
+    def __getattr__(self, name):
+        # delegate to unresolvables webdriver
+        return getattr(self.context, name)
 
 
 class PageObject(object):
@@ -103,23 +107,23 @@ class PageObject(object):
     }
 
     def __init__(self, drv):
-        self.driver = drv
+        self.context = drv
 
     def __getattr__(self, name):
         # delegate to unresolvables webdriver
-        return getattr(self.driver, name)
+        return getattr(self.context, name)
 
     def alert(self, timeout=5):
         """Wait and return alert"""
         page_logger.debug('Switching to alert.')
-        WebDriverWait(self.driver, timeout).until(EC.alert_is_present())
-        return self.driver.switch_to.alert
+        WebDriverWait(self.context, timeout).until(EC.alert_is_present())
+        return self.context.switch_to.alert
 
     def wait_ajax(self, lib='JQUERY', timeout=30):
         """Run AJAX call and wait for returning"""
         page_logger.debug('Waiting for AJAX using %s' % lib)
         js = self.wait_ajax_script.get(lib, 'return true;')
-        WebDriverWait(self.driver, timeout).until(
+        WebDriverWait(self.context, timeout).until(
             lambda driver: driver.execute_script(js))
 
     def goto(self, next_page):
@@ -132,142 +136,49 @@ class PageObject(object):
         path, cls = next_page.rsplit('.', 1)
         m = import_module(path)
         cls = getattr(m, cls)
-        return cls(self.driver)
+        return cls(self.context)
 
 
-class TableMeta(type):
-    """
-    Meta class for Table.
-    This class will solidify all columns selectors into cls._columns
-    """
+class PageTable(PageComponent):
 
-    def __new__(cls, name, bases, attrs):
-        page_logger.debug('Installing columns to table <%s>' % name)
-        _columns = {}
-        for n, attr in attrs.items():
-            if not isinstance(attr, TableColumn):
-                continue
-            page_logger.debug('%d %s...' % (attr.index, n))
-            attr.name = n
-            _columns[attr.index] = attr
-        attrs['_columns'] = _columns
-        page_logger.debug('Installation done <%s>' % name)
-        return type.__new__(cls, name, bases, attrs)
-
-
-class TableColumn(object):
-    """
-    Define a table column.
-    index - the index of the cells find by the locator of Table.__cell__, start from 0
-    wrapper - a callable used to cook data from the element located by index
-    """
-
-    def __init__(self, index, wrapper=None):
-        self.name = ''
-        self.index = index
-        self.wrapper = wrapper
-
-    def fetch(self, e):
-        page_logger.debug('Fetching cell from row...')
-        if self.wrapper is not None:
-            page_logger.debug('Wrapping table cell...')
-            return self.wrapper(e)
-        else:
-            return e
-
-
-class TableRow(object):
-    """
-    A row in the table located by Table.__row__
-    """
-
-    def __init__(self, element, cell_loc, clmn_spec):
-        self.element = element
-        self.cell_loc = cell_loc
-        self.clmn_spec = clmn_spec
-
-    def __getattr__(self, name):
-        page_logger.debug('Getting cell by name: %s' % name)
-        cells = self.element.find_elements(*self.cell_loc)
-        page_logger.debug('%d cells located' % len(cells))
-        for i, c in enumerate(cells):
-            if self.clmn_spec[i].name == name:
-                return self.clmn_spec[i].fetch(c)
-        page_logger.debug('Not found.')
-        return None
+    __row_locator__ = ('', '')
+    __row_component__ = None
 
     def __getitem__(self, index):
-        page_logger.debug('Getting cell by index: %d' % index)
-        if index in self.clmn_spec:
-            cells = self.element.find_elements(*self.cell_loc)
-            page_logger.debug('%d cells located' % len(cells))
-            return self.clmn_spec[index].fetch(cells[index])
-        page_logger.debug('Not found')
-        return None
-
-    def __str__(self):
-        cells = self.element.find_elements(*self.cell_loc)
-        text = []
-        for i, c in self.clmn_spec.items():
-            text.append(str(self.clmn_spec[i].fetch(cells[i])))
-        return ' | '.join(text)
-
-
-class TableBase(object):
-    """
-    The base class of Table.
-    This class will convert a <table> element into a structured data table
-    """
-
-    __metaclass__ = TableMeta
-
-    def __init__(self, table):
-        page_logger.debug('Table created.')
-        self.table = table
-
-    def __getitem__(self, index):
-        page_logger.debug('Getting row %d' % index)
-        rows = self.table.find_elements(*self.__row__)
-        return TableRow(rows[index], self.__cell__, self._columns)
+        row = self._all_rows()[index]
+        return self.__row__['component'](row)
 
     def __len__(self):
-        return len(self.table.find_elements(*self.__row__))
+        return len(self._all_rows())
 
-    def search(self, once=False, **conditions):
-        """
-        A Generator yielding all matching rows
-        """
-        page_logger.debug('Searching table...')
+    def _all_rows(self):
+        return self.context.find_elements(*self.__row_locator__)
+
+    def query(self, once=False, **conditions):
+        page_logger.debug('Querying table...')
+        rows = self._all_rows()
+
         for k, v in conditions.items():
             if not callable(v):
                 conditions[k] = lambda x, ref=v: x.get_attribute('contentText') == ref
 
-        for i in range(len(self)):
+        for i, row in enumerate(rows):
             page_logger.debug('Checking row %d...' % i)
-            if not conditions:
-                page_logger.debug('No conditions supplied. Match.')
-                yield self[i]
 
-            if all((getattr(self[i], name) is not None and
-                    condition(getattr(self[i], name))
-                   for name, condition in conditions.items())):
+            if self.__row_component__:
+                row = self.__row_component__(row)
+
+            for a, c in conditions.items():
+                page_logger.debug(getattr(row, a).get_attribute('contentText'))
+
+            match = not conditions or all(cond(getattr(row, attr)) for (attr, cond) in conditions.items())
+
+            if match:
                 page_logger.debug('Found matching row: %d' % i)
-                yield self[i]
-                if once:
-                    page_logger.debug('Terminating immediately after found.')
-                    break
+                yield row
+            else:
+                page_logger.debug('No match.')
 
-            # match = True
-            # for name, condition in conditions.items():
-            #     page_logger.debug('Checking %s...' % name)
-            #     e = getattr(self[i], name)
-            #     if e is None or not condition(e):
-            #         page_logger.debug('Failed')
-            #         match = False
-            #         break
-            # if match:
-            #     page_logger.debug('Found matching row: %d' % i)
-            #     yield self[i]
-            #     if once:
-            #         page_logger.debug('Terminating immediately after found.')
-            #         break
+            if match and once:
+                page_logger.debug('Terminating immediately after found.')
+                break
--- test.py
+++ test.py
@@ -1,59 +1,100 @@
-from page_object import PageObject, PageElement, PageComponent
+from page_object import PageObject, PageElement, PageComponent, PageTable
 from selenium import webdriver
 import logging
 
 page_logger = logging.getLogger('PageObject')
 
 
-class TestHomePage(PageObject):
-    sign_in = PageElement('Sign in', by='link text')
-
-    def to_login(self):
-        self.sign_in.click()
-        return self.goto('test.TestLoginPage')
-
-
 class TestLoginPage(PageObject):
-    user = PageElement('email')
-    password = PageElement('passwd')
-    login_button = PageElement('SubmitLogin')
+    user = PageElement('input[name="email"]', by='css selector')
+    password = PageElement('input[name="password"]', by='css selector')
+    login_button = PageElement('button[type="submit"]', by='css selector')
 
     def login(self, user, password):
         self.user = user
         self.password = password
         self.login_button.click()
-        return self.goto('test.AccountPage')
+        return self.goto('test.DashboardPage')
 
 
-class AccountMenu(PageComponent):
-    order_history = PageElement('Order history and details', by='link text')
+class PageNavigator(PageComponent):
+    home = PageElement('DASHBOARD', by='link text')
+    frontend = PageElement('Frontend', by='link text')
+    configuration = PageElement('Configuration', by='link text')
+    settings = PageElement('Settings', by='link text')
+    bookings = PageElement('Bookings', by='link text')
 
-    target = {
-        'order_history': 'test.HistoryPage'
+    _links = {
+        'frontend': 'test.FrontendPage',
+        'bookings': 'test.BookingsPage'
     }
 
-    def click(self, name):
-        menu = getattr(self, name)
-        menu.click()
-        return self.target[name]
+
+class BasePage(PageObject):
+    navigator = PageElement('nav', by='tag name', component=PageNavigator)
+
+    def nav_to(self, *menus):
+        for m in menus:
+            getattr(self.navigator, m).click()
+            self.wait_ajax()
+        return self.goto(self.navigator._links[menus[-1]])
 
 
-class AccountPage(PageObject):
-    account_menu = PageElement('center_column', wrapper=AccountMenu)
-
-    def goto(self, name):
-        target = self.account_menu.click(name)
-        return self.goto(target)
-
-
-class HistoryPage(PageObject):
+class DashboardPage(BasePage):
     pass
+
+
+class FrontendPage(BasePage):
+    pass
+
+
+class BookingRow(PageComponent):
+    tick = PageElement('td:nth-child(1) input[type="checkbox"]', by='css selector')
+    number = PageElement('td:nth-child(2)', by='css selector')
+    id_ = PageElement('td:nth-child(3)', by='css selector')
+    reference = PageElement('td:nth-child(4)', by='css selector')
+    customer = PageElement('td:nth-child(5)', by='css selector')
+    module = PageElement('td:nth-child(6)', by='css selector')
+    date = PageElement('td:nth-child(7)', by='css selector')
+    total = PageElement('td:nth-child(8)', by='css selector')
+    paid = PageElement('td:nth-child(9)', by='css selector')
+    remaining = PageElement('td:nth-child(10)', by='css selector')
+    status_button = PageElement('td:nth-child(11)', by='css selector')
+    view_button = PageElement('td:nth-child(12) a', by='css selector')
+    edit_button = PageElement('td:nth-child(13) a', by='css selector')
+    delete_button = PageElement('td:nth-child(14) a', by='css selector')
+
+    def view(self):
+        self.view_button.click()
+        self.page.wait_ajax()
+        return 'test.BasePage'
+
+    def edit(self):
+        self.edit_button.click()
+        self.page.wait_ajax()
+        return 'test.BasePage'
+
+    def delete(self):
+        self.delete_button.click()
+        self.page.wait_ajax()
+
+
+class BookingTable(PageTable):
+    __row_locator__ = ('css selector', 'tbody tr')
+    __row_component__ = BookingRow
+
+
+class BookingsPage(BasePage):
+    print_button = PageElement('Print', by='link text')
+    export_csv = PageElement('Export into CSV', by='link text')
+    booking_table = PageElement('#content table', by='css selector', component=BookingTable)
 
 
 if __name__ == '__main__':
     drv = webdriver.Firefox()
-    drv.get('http://automationpractice.com/')
-    page = TestHomePage(drv)
-    page = page.to_login()
-    page = page.login('yyloginbin@gmail.com', '12345')
-    page = page.goto('order_history')
+    drv.get('https://www.phptravels.net/admin')
+    page = TestLoginPage(drv)
+    page = page.login('admin@phptravels.com', 'demoadmin')
+    page = page.nav_to('bookings')
+    row = list(page.booking_table.query(reference='1248', once=True))[0]
+    row.delete()
